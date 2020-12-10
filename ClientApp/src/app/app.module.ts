63b547cecf760304c5a46c78245674d2c24100d7
Anu Virtanen <virtanenanum@gmail.com>
2020-12-10 13:24:12 +0200

    Add health-check component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { HealthCheckComponent } from './health-check/health-check.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    HealthCheckComponent
     
   ],
   imports: [
@@ -21,7 +23,8 @@ import { HomeComponent } from './home/home.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      {path: 'health-check', component: HealthCheckComponent}
      
     ])
   ],
